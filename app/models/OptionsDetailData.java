b6a0ec3795aa46dc4a061ba53b15852ef1e78b04
Tilloo <stilloo@LM-SJC-00710513.gateway.2wire.net>
2012-03-22 23:58:23 -0700

    expiration date

--- app/models/OptionsDetailData.java
+++ app/models/OptionsDetailData.java
@@ -2,13 +2,15 @@ package models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
 
 public class OptionsDetailData {
 
 	public String ticker;
-	public Map<String,Double> optionsDatePrice = new HashMap<String,Double>();
+	public Map<String,Double> optionsDatePrice = new LinkedHashMap<String,Double>();
 	public List<Double> stockPrice = new ArrayList<Double>();
 	
 }
